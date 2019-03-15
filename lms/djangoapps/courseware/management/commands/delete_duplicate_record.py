802ba915163e98d5d5d16c0ed79dcb13e8246c89
qazi-fahid <qazi.fahid@arbisoft.com>
2019-03-15 08:44:25 +0000

    management command created for deleting duplicate record

--- lms/djangoapps/courseware/management/commands/delete_duplicate_record.py
+++ lms/djangoapps/courseware/management/commands/delete_duplicate_record.py
@@ -9,13 +9,16 @@ from opaque_keys.edx.keys import CourseKey
 from opaque_keys import InvalidKeyError
 from util.prompt import query_yes_no
 from progress.models import CourseModuleCompletion
+from django.contrib.auth.models import User
 
 
 class Command(BaseCommand):
     """
     Command class for course reference removal
     """
-    help = '''Deletes database records having reference to the specified course'''
+
+    def add_arguments(self, parser):
+        parser.add_argument('args', nargs='*')
 
     def handle(self, *args, **options):
         """
@@ -23,26 +26,30 @@ class Command(BaseCommand):
         """
         if len(args) != 3:
             raise CommandError("delete_record requires exactly three arguments: <course_id> |<content_id>| |username|")
-
         try:
-            course_key = CourseKey.from_string("resouces/res202/2019_T2")
+            course_key = CourseKey.from_string(args[0])
         except InvalidKeyError:
-            raise
+            raise Exception("Invalid course key")
 
-        content_id = 'i4x://resouces/res202/static_tab/0f3f7dc298584702bcc687294d7269a2'
-        username = 'fahid-sami'
-
+        content_id = args[1]
+        username = args[2]
         if username:
-            user_id = 7
-            if content_id
+            try:
+                user_id = User.objects.get(username=username).pk
+            except User.DoesNotExist:
+                raise Exception("User does not exists with the given username")
+
+            if content_id:
                 if query_yes_no("Deleting Duplicate records with references to course {0} and content {1} and"
                                 " username {2}. Confirm?".format(course_key, content_id, username), default="no"):
                     if query_yes_no("Are you sure. This action cannot be undone!", default="no"):
-                        eeeee = CourseModuleCompletion.objects.get(
-                            user_id = user_id,
-                            course_id = course_key,
-                            content_id = content_id,
+                        course_modules = CourseModuleCompletion.objects.filter(
+                            user_id=user_id,
+                            course_id=course_key,
+                            content_id=content_id,
                         )
-                        print(eeeee)
-
-                        print 'Duplicate record deleted!'
+                        if course_modules and len(course_modules) > 1:
+                            [course_module.delete() for course_module in course_modules]
+                            print('Duplicate records deleted!')
+                        else:
+                            print('No duplicates records exists')
