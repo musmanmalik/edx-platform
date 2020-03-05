7a5ebbb0813f668b7b159e34152ae1a48265237f
ihtram <m.ihtram@arbisoft.com>
2020-03-05 14:59:18 +0500

    commented diagnostic_feedback from installed apps

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -2903,7 +2903,7 @@ OPTIONAL_APPS = [
     ('problem_builder', 'openedx.core.djangoapps.content.course_overviews.apps.CourseOverviewsConfig'),
     ('edx_sga', None),
     # TODO : ironwood rebase - Fix this before merging ironwood into master
-    ('diagnostic_feedback.apps.DiagnosticFeedbackConfig', None),
+    #('diagnostic_feedback.apps.DiagnosticFeedbackConfig', None),
 
     # edx-ora2
     ('submissions', 'openedx.core.djangoapps.content.course_overviews.apps.CourseOverviewsConfig'),
