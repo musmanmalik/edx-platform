1edc91ab4c6e2b9129358000677338b09801d034
Attiya Ishaque <atiya.ishaq@arbisoft.com>
2020-04-13 18:35:47 +0500

    Fix unregistered celery task. (#1722)

--- cms/envs/common.py
+++ cms/envs/common.py
@@ -915,7 +915,11 @@ WEBPACK_CONFIG_PATH = 'webpack.prod.config.js'
 ################################# CELERY ######################################
 
 # Auto discover tasks fails to detect contentstore tasks
-CELERY_IMPORTS = ('cms.djangoapps.contentstore.tasks')
+CELERY_IMPORTS = (
+    'cms.djangoapps.contentstore.tasks',
+    'openedx.core.djangoapps.bookmarks.tasks',
+    'openedx.core.djangoapps.content.course_structures.tasks',
+)
 
 # Message configuration
 
